09972ab46789f44e0fe6054f9d6ed8d8d80e9a2c
Manoella <manoella@syncode.com.br>
2024-03-14 22:26:45 -0300

    Projeto de numero secreto JS

---
+++ app.js
@@ -0,0 +1,24 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto e menor que ${chute}`);
+        } else {
+            alert(`O numero secreto e maior que ${chute}`);
+        }
+    }
+    tentativas++;
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
